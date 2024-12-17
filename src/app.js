16837a5baf13205c804d2ad21c03f9f2b3c5b1ca
Lovnyth Valcin <lovnyth@gmail.com>
2024-12-17 01:22:22 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,40 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My phone", "The wi-fi", "My friend"];
+  let action = [
+    "ate my homework",
+    "battery died",
+    "signal was weak",
+    "cancelled on me"
+  ];
+  let when = ["this morning", "yesterday", "last night", "an hour ago"];
+
+  console.log(who);
+  console.log(action);
+  console.log(when);
 };
+
+function generateExcuse() {
+  let who = ["The dog", "My phone", "The wi-fi", "My friend"];
+  let action = [
+    "ate my homework",
+    "battery died",
+    "signal was weak",
+    "cancelled on me"
+  ];
+  let when = ["this morning", "yesterday", "last night", "an hour ago"];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  return `${who[whoIndex]} ${action[actionIndex]} ${when[whenIndex]}`;
+}
+
+function displayExcuse() {
+  let excuse = generateExcuse();
+  document.getElementById("excuse").innerHTML = excuse;
+}
+window.onload = displayExcuse;
